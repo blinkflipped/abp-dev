16d924cac7e15d73675dbc888141bdacdd6d6c1d
Mónica del Corral <monica.delcorral@wildoom.com>
2018-12-14 12:44:39 +0100

    Open URL and Archivo in blank window.

--- style.js
+++ style.js
@@ -342,14 +342,18 @@ abpApp.currentSectionInBookIndex = function(currentID) {
 
 }
 
-abpApp.openActivity = function(url, subunitID) {
+abpApp.openActivity = function(url, subunitID, type) {
 
     if (blink.isApp) {
-        blink.rest.openUrl('fullscreen', url);
+      blink.rest.openUrl('fullscreen', url);
     } else {
+      var openBlank = (type === 'url' || type === 'archivo');
+      if (openBlank) {
+        blink.rest.openUrl('fullscreen', url);
+      } else {
         blink.goToActivity(idcurso, subunitID);
+      }
     }
-
 }
 
 
@@ -571,7 +575,7 @@ abpApp.loadHomepage = function(data,updateHash) {
                 subunitUrl = subunit.url,
                 subunitType = subunit.type,
                 subunitIsMedia = (subunitType === 'img' || subunitType === 'musica' || subunitType === 'video'),
-                subunitOnClick = (subunitIsMedia) ? subunit.onclickTitle :"abpApp.openActivity('" + subunitUrl + "'," + subunitID + ")",
+                subunitOnClick = (subunitIsMedia) ? subunit.onclickTitle :"abpApp.openActivity('" + subunitUrl + "'," + subunitID + ", ' "+subunitType+"')",
                 subunitIsOnlyVisibleTeacher = subunit.onlyVisibleTeachers;
             if (subunitIsOnlyVisibleTeacher && !abpApp.config.isStudent || !subunitIsOnlyVisibleTeacher) {
               var tabListItem = document.createElement('li');
@@ -746,7 +750,7 @@ abpApp.loadUnit = function(data,currentUnit,activeAreaTeacher,updateHash) {
 
           var subunitType = subunit.type,
               subunitIsMedia = (subunitType === 'img' || subunitType === 'musica' || subunitType === 'video'),
-              subunitOnClick = (subunitIsMedia) ? subunit.onclickTitle : "abpApp.openActivity('" + subunitUrl + "'," + subunitID + ")",
+              subunitOnClick = (subunitIsMedia) ? subunit.onclickTitle : "abpApp.openActivity('" + subunitUrl + "'," + subunitID + ", '"+subunitType+"')",
               subunitUrlHTML = (abpApp.config.isStudent && (isSubunitLock)) ? 'class="abp-resources-list-item-inner abp-js--subunitLocked"' : 'class="abp-resources-list-item-inner" data-subunit-id="'+subunitID+'"',
               subunitTitleUrlHTML = (abpApp.config.isStudent && (isSubunitLock)) ? '<h3 class="abp-title-5">'+subunitTitle+'</h3>' : '<h3 class="abp-title-5 abp-js--gotoActivity" onclick="'+subunitOnClick+'">'+subunitTitle+'</h3>',
               subunitInnerHTML = '<article class="abp-resources-list-item-article '+subunitLockClass+'"> <a href="javascript:void(0)" '+subunitUrlHTML+'><div class="abp-resources-list-item-image"><div class="abp-resources-list-item-image-inner">'+subunitImageCode+'</div></div><div class="abp-resources-list-item-text"><div class="abp-resources-list-item-text-main"><div class="abp-resources-list-item-text-main-top">'+subunitAux1+subunitTitleUrlHTML+'</div><div class="abp-resources-list-item-text-main-bottom"><p>'+subunitDescription+'</p></div></div>'+subunitAux3+'</div></a></article>';
@@ -761,7 +765,7 @@ abpApp.loadUnit = function(data,currentUnit,activeAreaTeacher,updateHash) {
         } else if (subunitIsOnlyVisibleTeacher && !abpApp.config.isStudent) {
           var subunitType = subunit.type,
               subunitIsMedia = (subunitType === 'img' || subunitType === 'musica' || subunitType === 'video'),
-              subunitOnClick = (subunitIsMedia) ? subunit.onclickTitle : "abpApp.openActivity('" + subunitUrl + "'," + subunitID + ")",
+              subunitOnClick = (subunitIsMedia) ? subunit.onclickTitle : "abpApp.openActivity('" + subunitUrl + "'," + subunitID + ", '"+subunitType+"')",
               subunitTeachersTypeHTML = (subunitType !== '') ? '<span class="abp-resources-list-icon abp-resources-list-icon_'+subunitType+'" aria-hidden="true"></span>' : '',
               subunitTeachersUrlHTML = 'class="abp-resources-list-item-inner" onclick="'+subunitOnClick+'" data-subunit-id="'+subunitID+'"',
               subunitTeachersInnerHTML = '<article class="abp-resources-list-item-article"> <a href="javascript:void(0)" '+subunitTeachersUrlHTML+'><div class="abp-resources-list-item-image"><div class="abp-resources-list-item-image-inner">'+subunitTeachersTypeHTML+'</div></div><div class="abp-resources-list-item-text"><div class="abp-resources-list-item-text-main"><div class="abp-resources-list-item-text-main-top"><h3 class="abp-title-5">'+subunitTitle+'</h3></div><div class="abp-resources-list-item-text-main-bottom"><p>'+subunitDescription+'</p></div></div></div></a> </article>';
